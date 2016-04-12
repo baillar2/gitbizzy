6f4bc860ce382162f985a6ccda50b79ad0bc7f31
Sean Baillargeon <seanpbaillargeon@gmail.com>
2016-04-12 13:44:33 -0600

    first deploy

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ passport.use(new githubStrategy({
 	function(accessToken, refreshToken, profile, done){
 		// Finding User in database
 		// IF exists, log them in
-		// IF NOT, create them
+		// 
 		User.findOne({ github : profile.id }, function (err, user) {
 			
 			// Something went horribly wrong in the DB!!!!
